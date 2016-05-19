e6b9bc60fd948d8b2838d409b1c2f1333ce0748e
Hugo PIGEON <hugo.pigeon@etu.univ-nantes.fr>
2016-05-19 11:28:19 +0200

    Remove useless comments

--- src/main/java/org/polytech/polybigbalance/graphics/TextVertShader.glsl
+++ src/main/java/org/polytech/polybigbalance/graphics/TextVertShader.glsl
@@ -9,9 +9,6 @@ uniform mat4 MVP;
 
 void main()
 {
-	//vec2 vertexPosition_homoneneousspace = vertexPosition_screenspace - vec2(300, 400);
-	//vertexPosition_homoneneousspace /= vec2(300, 400);
-	//gl_Position =  vec4(vertexPosition_homoneneousspace, 0, 1);
 	gl_Position =  MVP * vec4(vertexPosition_screenspace, 0, 1);
 
 	UV = vertexUV;
