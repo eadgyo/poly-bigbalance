56067f8312f8705e18f3c601be1dd9c3a5d4af3b
Hugo PIGEON <hugo.pigeon@etu.univ-nantes.fr>
2016-05-19 15:24:17 +0200

    Add comments to Button class

--- src/main/java/org/polytech/polybigbalance/graphics/Button.java
+++ src/main/java/org/polytech/polybigbalance/graphics/Button.java
@@ -1,30 +1,46 @@
 package org.polytech.polybigbalance.graphics;
 
 import java.awt.*;
-import java.io.IOException;
 import java.nio.FloatBuffer;
 
+/**
+ * Defines a button made of a text inside a rectangle
+ */
 public class Button
 {
     private Rectangle rectangle;
     private Text2D text;
 
-    public Button(Vec2 position, String text, int font, Shader colorShader, Shader textShader)
+    /**
+     * @param position position of the top left corner
+     * @param text text to display inside the buttons
+     * @param font ID of the font to use for the text
+     * @param rectangleShader shader to use for the rectangle
+     * @param textShader shader to use for the text
+     */
+    public Button(Vec2 position, String text, int font, Shader rectangleShader, Shader textShader)
     {
         Vec2 textPosition = new Vec2(position.getX() + 5.0f, position.getY() + 5.0f);
         this.text = new Text2D(font, text, textPosition, 24, textShader);
 
         Vec2 textSize = this.text.getPixelSize();
         Vec2 rectangleSize = new Vec2(textSize.getX() + 10.0f, textSize.getY() + 10.0f);
-        this.rectangle = new Rectangle(position, rectangleSize, Color.GRAY, colorShader);
+        this.rectangle = new Rectangle(position, rectangleSize, Color.GRAY, rectangleShader);
     }
 
+    /**
+     * Displays the button
+     * @param mvpBuffer buffer containing the MVP matrix
+     */
     public void display(FloatBuffer mvpBuffer)
     {
         this.rectangle.display(mvpBuffer);
         this.text.display(mvpBuffer);
     }
 
+    /**
+     * Correctly deletes the buffers
+     */
     public void delete()
     {
         this.rectangle.delete();
