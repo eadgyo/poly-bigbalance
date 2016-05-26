62b8eb37f3f0daff44ce212410d1e5ace14f0533
ronan <ronanjamet.h@gmail.com>
2016-05-26 12:43:17 +0200

    Add Button

--- src/main/java/org/polytech/polybigbalance/graphics/base/GraphicsObject.java
+++
@@ -1,9 +0,0 @@
-package org.polytech.polybigbalance.graphics.base;
-
-import org.polytech.polybigbalance.graphics.Graphics;
-
-public abstract class GraphicsObject
-{
-    public GraphicsObject(int width, int height) {}
-    public abstract void render(Graphics g);
-}
