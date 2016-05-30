d68b2d8025ed7abce4564bfe2445ca94e36010b1
Hugo PIGEON <hugo.pigeon@etu.univ-nantes.fr>
2016-05-30 10:02:41 +0200

    Remove useless final

--- src/main/java/org/polytech/polybigbalance/Constants.java
+++ src/main/java/org/polytech/polybigbalance/Constants.java
@@ -6,15 +6,15 @@ import org.cora.graphics.manager.TextureManager;
 
 public interface Constants
 {
-    final String WINDOW_TITLE = "Poly Big Balance";
-    final int WINDOW_WIDTH = 800;
-    final int WINDOW_HEIGHT = 600;
+    String WINDOW_TITLE = "Poly Big Balance";
+    int WINDOW_WIDTH = 800;
+    int WINDOW_HEIGHT = 600;
 
-    final String[] MAIN_MENU_BUTTONS = { "New Game", "High scores", "How to play", "Credits", "Exit" };
+    String[] MAIN_MENU_BUTTONS = { "New Game", "High scores", "How to play", "Credits", "Exit" };
 
-    final String RESOURCES_PATH = "src/main/resources/";
+    String RESOURCES_PATH = "src/main/resources/";
 
-    final Graphics g = new Graphics(WINDOW_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT, true, true);
+    Graphics g = new Graphics(WINDOW_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT, true, true);
 
-    final Surface TEXT_FONT_SURFACE = TextureManager.getInstance().loadTexture(RESOURCES_PATH + "font.bmp");
+    Surface TEXT_FONT_SURFACE = TextureManager.getInstance().loadTexture(RESOURCES_PATH + "font.bmp");
 }
