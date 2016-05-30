014ee5b907b193b3b2f4ecd168ab872457d5cca0
Hugo PIGEON <hugo.pigeon@etu.univ-nantes.fr>
2016-05-30 22:36:34 +0200

    Change level 1

--- src/main/java/org/polytech/polybigbalance/layers/Level1.java
+++ src/main/java/org/polytech/polybigbalance/layers/Level1.java
@@ -20,13 +20,42 @@ public class Level1 extends Level
     {
         super.initialize();
 
-        this.baseRectangles.put(new Rectangle(new Vector2D(Constants.WINDOW_WIDTH / 2, Constants.WINDOW_HEIGHT / 2),
-                new Vector2D(200, 100), 0.0f), new RigidBody());
+        this.baseRectangles.put(new Rectangle(new Vector2D(Constants.WINDOW_WIDTH / 2, Constants.WINDOW_HEIGHT - 75),
+                new Vector2D(50, 50), 0.0f), new RigidBody());
+
+        this.playerRectangles.put(new Rectangle(new Vector2D(Constants.WINDOW_WIDTH / 2, Constants.WINDOW_HEIGHT - 125),
+                new Vector2D(50, 50), 0.0f), new RigidBody());
+
+        this.playerRectangles.put(new Rectangle(new Vector2D(Constants.WINDOW_WIDTH / 2, Constants.WINDOW_HEIGHT - 175),
+                new Vector2D(50, 50), 0.0f), new RigidBody());
+
+        this.playerRectangles.put(new Rectangle(new Vector2D(Constants.WINDOW_WIDTH / 2, Constants.WINDOW_HEIGHT - 225),
+                new Vector2D(300, 50), 0.0f), new RigidBody());
+
+        this.playerRectangles.put(new Rectangle(new Vector2D(Constants.WINDOW_WIDTH / 2, Constants.WINDOW_HEIGHT - 275),
+                new Vector2D(50, 50), 0.0f), new RigidBody());
+
+        this.playerRectangles.put(new Rectangle(new Vector2D(Constants.WINDOW_WIDTH / 2, Constants.WINDOW_HEIGHT - 325),
+                new Vector2D(100, 50), 0.0f), new RigidBody());
 
         for(Rectangle rectForm : this.baseRectangles.keySet())
         {
             rectForm.updateCenter();
             RigidBody rect = this.baseRectangles.get(rectForm);
+
+            rect.setMass(100.0f);
+            rect.setForm(rectForm);
+            rect.setPosition(rectForm.getCenter());
+            rect.initPhysics();
+
+            this.physEngine.addElement(rect);
+            this.physEngine.addForce(rect, this.gravity);
+        }
+
+        for(Rectangle rectForm : this.playerRectangles.keySet())
+        {
+            rectForm.updateCenter();
+            RigidBody rect = this.playerRectangles.get(rectForm);
 
             rect.setMass(100.0f);
             rect.setForm(rectForm);
