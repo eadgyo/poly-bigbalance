23f3d14e8c3b53c10bd7ec9c5d0d37519803ad89
ronan <ronanjamet.h@gmail.com>
2016-06-02 23:46:27 +0200

    Add QuadTreeRenderer

---
+++ src/main/java/org/polytech/polybigbalance/game/QuadTreeRenderer.java
@@ -0,0 +1,25 @@
+package org.polytech.polybigbalance.game;
+
+import org.cora.graphics.graphics.Graphics;
+import org.cora.physics.Engine.QuadTree;
+
+/**
+ * Created by ronan-h on 02/06/16.
+ */
+public class QuadTreeRenderer
+{
+    public static void drawQuadTree(Graphics g, QuadTree quadTree)
+    {
+        g.drawForm(quadTree.getRect());
+
+        QuadTree nodes[] = quadTree.getNodes();
+
+        if (nodes[0] != null)
+        {
+            for (int i = 0; i < nodes.length; i++)
+            {
+                drawQuadTree(g, nodes[i]);
+            }
+        }
+    }
+}
